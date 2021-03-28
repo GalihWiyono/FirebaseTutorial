69f32d1ba5684010e9d7596235d3db7ee2d83118
Lih <galihputra2201@gmail.com>
2021-03-28 15:43:02 +0700

    adding search feature for searching menu

--- app/src/main/java/com/example/firebaseimage/ImagesActivity.java
+++ app/src/main/java/com/example/firebaseimage/ImagesActivity.java
@@ -6,12 +6,16 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
@@ -22,6 +26,9 @@ public class ImagesActivity extends AppCompatActivity {
     private RecyclerView rvView;
     private ImageAdapter adapter;
 
+    private ImageButton btnSearch;
+    private EditText txtSearch;
+
     private DatabaseReference databaseReference;
     private List<Barang> lbrg;
 
@@ -29,6 +36,9 @@ public class ImagesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_images);
+
+        btnSearch = findViewById(R.id.btnSearch);
+        txtSearch = findViewById(R.id.txtSearch);
 
         rvView = findViewById(R.id.recycle_view);
         rvView.setHasFixedSize(true);
@@ -45,9 +55,7 @@ public class ImagesActivity extends AppCompatActivity {
                    Barang brg = item.getValue(Barang.class);
                    lbrg.add(brg);
                }
-
-               adapter = new ImageAdapter(ImagesActivity.this, lbrg);
-               rvView.setAdapter(adapter);
+                viewData(lbrg);
             }
 
             @Override
@@ -55,5 +63,38 @@ public class ImagesActivity extends AppCompatActivity {
                 Toast.makeText(ImagesActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SearchData(txtSearch.getText().toString());
+            }
+        });
+    }
+
+    private void SearchData(String textSearch) {
+        lbrg.clear();
+        Toast.makeText(this, "Data Dicari", Toast.LENGTH_SHORT).show();
+        Query query = databaseReference.orderByChild("nama").startAt(textSearch).endAt(textSearch + "\uf8ff");
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot item : snapshot.getChildren()) {
+                    Barang brg = item.getValue(Barang.class);
+                    lbrg.add(brg);
+                }
+                viewData(lbrg);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(ImagesActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void viewData(List list) {
+        adapter = new ImageAdapter(ImagesActivity.this, list);
+        rvView.setAdapter(adapter);
     }
 }
